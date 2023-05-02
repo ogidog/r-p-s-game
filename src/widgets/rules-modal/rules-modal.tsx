f82cb63ce8a6b976017b52ffdfd4ed9c334d6668
ogidog <ogidog@mail.ru>
2023-05-03 00:29:35 +0700

    Added result form

---
+++ src/widgets/rules-modal/rules-modal.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import styled from "styled-components";
+
+const StyledContainer = styled.div`
+  position: absolute;
+  
+  width: 100vw;
+  height: 100vh;
+  
+`
+
+export const RulesModal = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
